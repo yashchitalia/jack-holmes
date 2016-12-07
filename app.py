173ad8e5f35af270d460131a30b9050e7fc6c118
yashchitalia <yashc@yashc.fake>
2016-12-07 10:08:22 -0500

    hopefully last bugfix

--- app.py
+++ app.py
@@ -179,13 +179,16 @@ def generateCoursePlan():
                 course_number = str(course_number_tuple)
                 print "Course number"+ course_number
                 #Extract Instructor Data for this course
-                curr_instructor = omscs_dat[course_number]["Instructor"]
-                easiness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["easiness_query"]]) 
-                helpfulness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["helpfulness_query"]]) 
-                quality_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["quality_query"]]) 
-                curr_score = (easiness_rating*easiness_preference +
-                              helpfulness_rating*helpfulness_preference +
-                              quality_rating*quality_preference)
+                try:
+                    curr_instructor = omscs_dat[course_number]["Instructor"]
+                    easiness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["easiness_query"]]) 
+                    helpfulness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["helpfulness_query"]]) 
+                    quality_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["quality_query"]]) 
+                    curr_score = (easiness_rating*easiness_preference +
+                                  helpfulness_rating*helpfulness_preference +
+                                  quality_rating*quality_preference)
+                except:
+                    curr_score = 0
                 print curr_score
                 if curr_score > max_score:
                     max_score = curr_score
@@ -195,16 +198,16 @@ def generateCoursePlan():
                 curr_score = 0
                 for course_number in course_number_tuple:
                     #Extract Instructor Data for this course
-                    print course_number
-                    curr_instructor = omscs_dat[course_number]["Instructor"]
-                    print curr_instructor
-                    easiness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["easiness_query"]]) 
-                    helpfulness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["helpfulness_query"]]) 
-                    quality_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["quality_query"]]) 
-                    curr_score += (easiness_rating*easiness_preference +
-                                  helpfulness_rating*helpfulness_preference +
-                                  quality_rating*quality_preference)
-                    print curr_score
+                    try:
+                        curr_instructor = omscs_dat[course_number]["Instructor"]
+                        easiness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["easiness_query"]]) 
+                        helpfulness_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["helpfulness_query"]]) 
+                        quality_rating=float(rmp_data[instructor_names.index(curr_instructor)][DICT_OF_INSTRUCTOR_QUERIES["quality_query"]]) 
+                        curr_score += (easiness_rating*easiness_preference +
+                                      helpfulness_rating*helpfulness_preference +
+                                      quality_rating*quality_preference)
+                    except:
+                        curr_score += 0
                 if curr_score > max_score:
                     max_score = curr_score
                     best_tuple = course_number_tuple
